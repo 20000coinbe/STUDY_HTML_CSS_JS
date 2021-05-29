1663a4409c66f7f081de2c5cc4f6b53f36a57cec
Neo <kimdks12@gmail.com>
2021-05-29 21:28:27 +0900

    Object test

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 const a = 1;
+
+const obj = {
+  name: "neo",
+  age: 23,
+  from: {
+    jeonbuk: "jeonju",
+  },
+};
+
+console.log(obj.name); // neo
+console.log(obj.age); // 23
+console.log(obj.from); // { jeonbuk: 'jeonju' }
+console.log(obj.from.jeonbuk); // jeonju
